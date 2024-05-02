ff9b67ae45a0e31920d7dc52956f181ac1b8c068
Newenergystar <workingenoma@gmail.com>
2024-05-02 13:50:50 +0100

    Made Footer and Header folders and other folders

---
+++ src/pages/Admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Admin.module.scss";
+
+const j = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default j
---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Cart.module.scss";
+
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
