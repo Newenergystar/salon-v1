1560fe77a9d3c545ad1b6d5f3d1eb6796fbc936c
Newenergystar <workingenoma@gmail.com>
2024-08-06 09:07:54 +0100

    Project Finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 export {default as Home} from "./Home/Home";
 
 export {default as Contact} from "./Contact/Contact";
+export {default as Reviews} from "./Reviews/Reviews";
+export {default as Services} from "./Services/Services";
+export {default as Products} from "./Products/Products";
+export {default as Book} from "./Book/Book";
+export {default as Admin} from "./Admin/Admin";
+export {default as NotFound} from "./NotFound/NotFound"
+export {default as Appointment} from "./Appointment/Appointment"
